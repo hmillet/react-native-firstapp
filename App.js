467cb79bad4cd5a3843139a05208951b204453ec
Hervé MILLET <hmillet@gmail.com>
2018-01-22 16:53:57 +0100

    2001

--- App.js
+++ App.js
@@ -4,54 +4,195 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
-  Platform,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
+  View,
+  ScrollView,
+  Image,
+  Button
+} from "react-native";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component<{}> {
+export default class App extends Component {
   render() {
+    console.log("- start App-");
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to WAYA TECH !
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <ScrollView style={styles.mainContainer}>
+        <View style={styles.headerContainer}>
+          <Text style={styles.headerText}>
+            2001, l'odyssée de l'espace (1968)
+          </Text>
+        </View>
+        <View style={styles.part2Container}>
+          <Text style={styles.part2Text1}>
+            2001, l'odyssée de l'espace{" "}
+            <Text style={styles.part2Text2}>(1968)</Text>
+          </Text>
+          <Text style={styles.part2Text3}>
+            2001: A Space Odyssey{" "}
+            <Text style={styles.part2Text4}>(original title)</Text>
+          </Text>
+          <Text style={styles.part2Text3}>
+            Tous publics{"  |  "}2h 29min{"  |  "}Adventure, Sci-Fi
+          </Text>
+        </View>
+        <View style={styles.part3Container}>
+          <View style={styles.part3Container1}>
+            <Image
+              style={{ width: 160, height: 250 }}
+              source={{
+                uri:
+                  "http://www.dvdclassik.com/upload/images/affiches/2001-l-odyssee-de-l-espace.jpeg"
+              }}
+            />
+          </View>
+          <View style={styles.part3Container2}>
+            <Text style={styles.part3Text1} numberOfLines={3}>
+              Humanity finds a mysterious, obviously artificial object buried
+              beneath the Lunar surface and, with the intelligent computer
+              H.A.L. 9000, sets off on a quest
+            </Text>
+            <Button
+              onPress={() => alert("toto")}
+              title="+ Add to Watchlist"
+              color="#3e90eb"
+              style={{ height: 40, fontSize: 25 }}
+            />
+          </View>
+        </View>
+        <View style={styles.part4Container}>
+          <View style={styles.part4Container1}>
+            <Image
+              style={{ width: 60, height: 60 }}
+              source={{
+                uri:
+                  "http://icons.iconarchive.com/icons/tatice/cristal-intense/128/Etoile-icon.png"
+              }}
+            />
+            <View
+              style={{
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "space-around"
+              }}
+            >
+              <Text style={{ fontSize: 40, color: "#ffffff" }}>
+                8.3 <Text style={{ fontSize: 20, color: "#aaaaaa" }}>/10</Text>
+              </Text>
+              <Text style={{ fontSize: 20, color: "#bbbbbb" }}>454 834</Text>
+            </View>
+            <Image
+              style={{ width: 60, height: 60 }}
+              source={{
+                uri: "http://images.clipartpanda.com/star-clipart-bTyExdEjc.png"
+              }}
+            />
+            <View>
+              <Text>Rate This</Text>
+            </View>
+          </View>
+          <View style={styles.part4Container2} />
+        </View>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flexDirection: "column",
+    backgroundColor: "#373737"
   },
-  welcome: {
+  headerContainer: {
+    height: 50,
+    borderBottomColor: "#bbb",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-end"
+  },
+  headerText: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    color: "#ffffff",
+    paddingBottom: 5
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  part2Container: {
+    borderBottomColor: "#bbb",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    flex: 1,
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+    paddingHorizontal: 30,
+    paddingVertical: 12
   },
+  part2Text1: {
+    fontSize: 40,
+    color: "#ffffff",
+    paddingBottom: 5
+  },
+  part2Text2: {
+    fontSize: 30,
+    color: "#aaaaaa",
+    paddingBottom: 5
+  },
+  part2Text3: {
+    fontSize: 25,
+    color: "#dddddd",
+    paddingBottom: 5
+  },
+  part2Text4: {
+    fontSize: 25,
+    color: "#dddddd",
+    paddingBottom: 5,
+    fontStyle: "italic"
+  },
+  part3Container: {
+    backgroundColor: "#1f1f1f",
+    height: 300,
+    flexDirection: "row",
+    borderBottomColor: "#bbb",
+    borderBottomWidth: StyleSheet.hairlineWidth
+  },
+  part3Container1: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  part3Container2: {
+    flex: 2,
+    flexDirection: "column",
+    paddingHorizontal: 10,
+    paddingVertical: 20
+  },
+  part3Text1: {
+    color: "#ffffff",
+    fontSize: 30,
+    lineHeight: 50,
+    paddingBottom: 20
+  },
+  part4Container: {
+    backgroundColor: "#1f1f1f",
+    height: 100,
+    flexDirection: "row",
+    borderBottomColor: "#bbb",
+    borderBottomWidth: StyleSheet.hairlineWidth
+  },
+  part4Container1: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    borderRightColor: "#bbb",
+    borderRightWidth: StyleSheet.hairlineWidth,
+    padding: 10
+  },
+  part4Container2: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
